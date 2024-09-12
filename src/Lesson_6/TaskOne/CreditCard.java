84d75b508acaea5b4c11df00050d3b8a3c8d3fc0
Anastasia Ivanova <anastasiaroubo@gmail.com>
2024-09-13 00:00:19 +0300

    fix: Lesson6 - TaskOne. Added the method to print card info, partially created a credit card object

--- src/Lesson_6/TaskOne/CreditCard.java
+++ src/Lesson_6/TaskOne/CreditCard.java
@@ -3,9 +3,26 @@ package Lesson_6.TaskOne;
 import java.util.Scanner;
 
 public class CreditCard {
-    public long id;
-    public String accountNumber;
-    public double currentAccountAmount;
+    private long id;
+    private String accountNumber;
+    private double currentAccountAmount;
+
+
+
+     {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the current amount on Your card: ");
+        this.currentAccountAmount = scanner.nextDouble();
+        if (currentAccountAmount < 0) {
+            System.out.println("The amount must be more than 0. Please try again.");
+            return;
+        }
+    }
+
+    public CreditCard(long id, String accountNumber) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+    }
 
 
     public void creditOfFunds(double NewAmount) {
@@ -18,4 +35,9 @@ public class CreditCard {
         System.out.println("Your new amount is " + currentAccountAmount);
     }
 
+    public void printCardInfo() {
+        System.out.println("Card number: " + accountNumber + "," + "Current Account Amount: " + currentAccountAmount);
+    }
+
 }
+
