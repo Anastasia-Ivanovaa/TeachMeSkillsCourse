0b38cbaaf6058497d89c2e73b2bed9fc876d70c4
Anastasia Ivanova <anastasiaroubo@gmail.com>
2024-09-21 00:49:45 +0300

    dev: Lesson8 - TaskTwo: changed the conditions of method in base converter.

--- src/Lesson_8/TaskTwo/BaseConverter.java
+++ src/Lesson_8/TaskTwo/BaseConverter.java
@@ -1,23 +1,36 @@
 package Lesson_8.TaskTwo;
 
+import java.util.Scanner;
+
 public class BaseConverter {
-    public double convert(double value, Type type) {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter temperature value (for example, 10 C): ");
+        convert(scanner.nextDouble(), Type.valueOf(scanner.next()));
+    }
+
+    public static double convert(double value, Type type) {
         switch (type) {
-            case CELSIUS -> {
-                return value;
+            case C -> {
+                double fahrenheit = Math.round(1.8 * value + 32);
+                double kelvin = Math.round(value + 273.15);
+                System.out.println(fahrenheit + " " + Type.F.getLetter() + "\n" + kelvin + " " + Type.K.getLetter());
             }
-            case KELVIN -> {
-                return value + 273.15;
+            case K -> {
+                double celsius = Math.round(value - 273.15);
+                double fahrenheit = Math.round(1.8 * celsius + 32);
+                System.out.println(celsius + " " + "C" + "\n" + fahrenheit + " " + "F");
             }
-            case FAHRENHEIT -> {
-                return 1.8 * value + 32;
+            case F -> {
+                double celsius = Math.round(value / 1.8 - 32);
+                double kelvin = Math.round(celsius + 273.15);
+                System.out.println(celsius + "C" + "\n" + kelvin + "K");
             }
-            default ->
-            {
+            default -> {
                 return 0;
             }
         }
-
-
+        return value;
     }
 }
--- src/Lesson_8/TaskTwo/Type.java
+++ src/Lesson_8/TaskTwo/Type.java
@@ -1,8 +1,19 @@
 package Lesson_8.TaskTwo;
 
 public enum Type {
-    CELSIUS,
-    FAHRENHEIT,
-    KELVIN;
+    C("C"),
+    F("F"),
+    K("K");
 
+    private final String letter;
+
+    public String getLetter() {
+        return letter;
     }
+
+    Type(String letter) {
+        this.letter = letter;
+    }
+
+
+}
