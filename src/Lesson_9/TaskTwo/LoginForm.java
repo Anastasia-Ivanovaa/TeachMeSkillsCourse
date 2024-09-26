c3a41b48e0835b5ef43391c7c142bc4fc8de734a
Anastasia Ivanova <anastasiaroubo@gmail.com>
2024-09-26 23:20:04 +0300

    feat: Lesson9 - TaskTwo: changed login method - removed new objects.

--- src/Lesson_9/TaskTwo/LoginForm.java
+++ src/Lesson_9/TaskTwo/LoginForm.java
@@ -15,7 +15,7 @@ public class LoginForm {
         System.out.println("Confirm password: ");
         String confirmPassword = scanner.nextLine();
 
-        if(login(login, password, confirmPassword)){
+        if (login(login, password, confirmPassword)) {
             System.out.println("You are successfully logged.");
         } else {
             System.out.println("You haven't logged.");
@@ -24,25 +24,33 @@ public class LoginForm {
 
     public static boolean login(String login, String password, String confirmPassword) {
 
+        String exceptionLogin = null;
+        String exceptionPassword = null;
+
         try {
-            WrongLoginException exceptionOne = new WrongLoginException("The login cannot contain more than 20 characters. Please try again.");
-            if (login.length() > 20)
-                throw exceptionOne;
-            WrongLoginException exceptionTwo = new WrongLoginException("The login cannot contain space(s). Please Try again. ");
-            if (login.contains(" "))
-                throw exceptionTwo;
-            WrongPasswordException exceptionThree = new WrongPasswordException("The password cannot contain more than 20 characters. Please try again.");
-            if (password.length() > 20)
-                throw exceptionThree;
-            WrongPasswordException exceptionFour = new WrongPasswordException("The password cannot contain space(s). Please Try again.");
-            if (password.contains(" "))
-                throw exceptionFour;
-            WrongPasswordException exceptionFive = new WrongPasswordException("The password must contain '1'.");
-            if (!password.contains("1"))
-                throw exceptionFive;
-            WrongPasswordException exceptionSix = new WrongPasswordException("Passwords don't match, please try again.");
-            if (!password.equals(confirmPassword))
-                throw exceptionSix;
+            if (login.length() > 20) {
+                exceptionLogin = "The login cannot contain more than 20 characters. Please try again.";
+            } else if (login.contains(" ")) {
+                exceptionLogin = "The login cannot contain space(s). Please Try again. ";
+            }
+
+            if (password.length() > 20) {
+                exceptionPassword = "The password cannot contain more than 20 characters. Please try again.";
+            } else if (password.contains(" ")) {
+                exceptionPassword = "The password cannot contain space(s). Please Try again.";
+            } else if (!password.contains("1")) {
+                exceptionPassword = "The password must contain '1'.";
+            } else if (!password.equals(confirmPassword)) {
+                exceptionPassword = "Passwords don't match, please try again.";
+            }
+
+            if (exceptionLogin != null) {
+                WrongLoginException e = new WrongLoginException(exceptionLogin);
+                throw e;
+            } else if (exceptionPassword != null) {
+                WrongPasswordException e = new WrongPasswordException(exceptionPassword);
+                throw e;
+            }
             return true;
         } catch (WrongLoginException | WrongPasswordException e) {
             System.out.println(e.getMessage());
